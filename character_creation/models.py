5c390c6eeac4e863ac27c2e412a9855df1a6671c
Nevan Masterson <nevanmasterson1@gmail.com>
2024-01-16 20:16:26 +0000

    further developed models.py, and began implementation of script that will pull data from srd api and populate the database with it.

--- character_creation/models.py
+++ character_creation/models.py
@@ -55,6 +55,43 @@ class damage_types():
     def get_types(self):
         return self.DAMAGE_TYPES
 
+class Race(models.Model):
+    """Model that describes how races are stored in the database."""
+    name = models.CharField(unique=True)
+    size = models.CharField(max_length=1, choices=sizes.SIZE_CHOICES)
+    size_desc = models.TextField()
+    speed_walking = models.PositiveSmallIntegerField(validators=[validate_mod_five])
+    speed_flying = models.PositiveSmallIntegerField(validators=[validate_mod_five])
+    speed_burrowing = models.PositiveSmallIntegerField(validators=[validate_mod_five])
+    speed_swimming = models.PositiveSmallIntegerField(validators=[validate_mod_five])
+    speed_climbing = models.PositiveSmallIntegerField(validators=[validate_mod_five])
+    desc_short = models.CharField(max_length=500) # Arbitrary limit, may need revising later
+    desc_long = models.CharField()
+    age_desc = models.CharField()
+
+class Subrace(models.Model):
+    name = models.CharField(unique=True)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+
+
+class Trait(models.Model):
+    name = models.CharField(unique=True)
+    races = models.ManyToManyField(Race)
+    desc = models.TextField()
+
+class Feature():
+    name = models.CharField(unique=True)
+
+class RawStats(models.Model):
+    """Model for holding the raw statistical modifiers for a character's abilities."""
+    strength     = models.PositiveSmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(20)])
+    constitution = models.PositiveSmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(20)])
+    dexterity    = models.PositiveSmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(20)])
+    wisdom       = models.PositiveSmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(20)])
+    intelligence = models.PositiveSmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(20)])
+    charisma     = models.PositiveSmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(20)])
+
+
 class Character(models.Model):
     """Model that describes how player character information is stored. Many-to-one relationship with User. A character
     can belong to many classes (through multi-classing)."""
@@ -62,6 +99,9 @@ class Character(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     raw_stats = models.ForeignKey(RawStats, on_delete=models.CASCADE)
     character_level = models.PositiveSmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(20)])
+    inventory = models.TextField()
+    race = models.ForeignKey(Race, on_delete=models.RESTRICT)
+    subrace = models.ForeignKey(Subrace, on_delete=models.RESTRICT)
 
 
 class CharacterProficiencies(models.Model):
@@ -88,60 +128,46 @@ class CharacterProficiencies(models.Model):
 class CharacterProficiencyCustomValues(models.Model):
     """Model for representing custom values for proficiencies, should the player wish to override the values for reasons
      such as magical items, features, or for fun."""
-    acrobatics_val = models.SmallIntegerField(null=true, default=null, validators=[MinValueValidator(-100), MaxValueValidator(100)])
-    animal_handling_val = models.SmallIntegerField(null=true, default=null, validators=[MinValueValidator(-100), MaxValueValidator(100)])
-    arcana_val = models.SmallIntegerField(null=true, default=null, validators=[MinValueValidator(-100), MaxValueValidator(100)])
-    athletics_val = models.SmallIntegerField(null=true, default=null, validators=[MinValueValidator(-100), MaxValueValidator(100)])
-    deception_val = models.SmallIntegerField(null=true, default=null, validators=[MinValueValidator(-100), MaxValueValidator(100)])
-    history_val = models.SmallIntegerField(null=true, default=null, validators=[MinValueValidator(-100), MaxValueValidator(100)])
-    insight_val = models.SmallIntegerField(null=true, default=null, validators=[MinValueValidator(-100), MaxValueValidator(100)])
-    intimidation_val = models.SmallIntegerField(null=true, default=null, validators=[MinValueValidator(-100), MaxValueValidator(100)])
-    investigation_val = models.SmallIntegerField(null=true, default=null, validators=[MinValueValidator(-100), MaxValueValidator(100)])
-    medicine_val = models.SmallIntegerField(null=true, default=null, validators=[MinValueValidator(-100), MaxValueValidator(100)])
-    nature_val = models.SmallIntegerField(null=true, default=null, validators=[MinValueValidator(-100), MaxValueValidator(100)])
-    perception_val = models.SmallIntegerField(null=true, default=null, validators=[MinValueValidator(-100), MaxValueValidator(100)])
-    performance_val = models.SmallIntegerField(null=true, default=null, validators=[MinValueValidator(-100), MaxValueValidator(100)])
-    persuasion_val = models.SmallIntegerField(null=true, default=null, validators=[MinValueValidator(-100), MaxValueValidator(100)])
-    religion_val = models.SmallIntegerField(null=true, default=null, validators=[MinValueValidator(-100), MaxValueValidator(100)])
-    sleight_of_hand_val = models.SmallIntegerField(null=true, default=null, validators=[MinValueValidator(-100), MaxValueValidator(100)])
-    stealth_val = models.SmallIntegerField(null=true, default=null, validators=[MinValueValidator(-100), MaxValueValidator(100)])
-    survival_val = models.SmallIntegerField(null=true, default=null, validators=[MinValueValidator(-100), MaxValueValidator(100)])
-
-class ClassInstance(models.Model):
-    """Model for representing a particular instance of a class. For instance, this model may be used to store
-    information about a level 14 barbarian with a subclass in path of the bezerker."""
-    character = models.ForeignKey(Character, on_delete=models.CASCADE, null=false, blank=false)
-    class_type = models.ForeignKey(CharacterClass, on_delete=models.CASCADE, null=false, blank=false)
-    subclass_type = models.ForeignKey(CharacterSubclass, on_delete=models.CASCADE)
-    class_level = models.PositiveSmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(20)])
-
+    acrobatics_val = models.SmallIntegerField(null=True, default=None, validators=[MinValueValidator(-100), MaxValueValidator(100)])
+    animal_handling_val = models.SmallIntegerField(null=True, default=None, validators=[MinValueValidator(-100), MaxValueValidator(100)])
+    arcana_val = models.SmallIntegerField(null=True, default=None, validators=[MinValueValidator(-100), MaxValueValidator(100)])
+    athletics_val = models.SmallIntegerField(null=True, default=None, validators=[MinValueValidator(-100), MaxValueValidator(100)])
+    deception_val = models.SmallIntegerField(null=True, default=None, validators=[MinValueValidator(-100), MaxValueValidator(100)])
+    history_val = models.SmallIntegerField(null=True, default=None, validators=[MinValueValidator(-100), MaxValueValidator(100)])
+    insight_val = models.SmallIntegerField(null=True, default=None, validators=[MinValueValidator(-100), MaxValueValidator(100)])
+    intimidation_val = models.SmallIntegerField(null=True, default=None, validators=[MinValueValidator(-100), MaxValueValidator(100)])
+    investigation_val = models.SmallIntegerField(null=True, default=None, validators=[MinValueValidator(-100), MaxValueValidator(100)])
+    medicine_val = models.SmallIntegerField(null=True, default=None, validators=[MinValueValidator(-100), MaxValueValidator(100)])
+    nature_val = models.SmallIntegerField(null=True, default=None, validators=[MinValueValidator(-100), MaxValueValidator(100)])
+    perception_val = models.SmallIntegerField(null=True, default=None, validators=[MinValueValidator(-100), MaxValueValidator(100)])
+    performance_val = models.SmallIntegerField(null=True, default=None, validators=[MinValueValidator(-100), MaxValueValidator(100)])
+    persuasion_val = models.SmallIntegerField(null=True, default=None, validators=[MinValueValidator(-100), MaxValueValidator(100)])
+    religion_val = models.SmallIntegerField(null=True, default=None, validators=[MinValueValidator(-100), MaxValueValidator(100)])
+    sleight_of_hand_val = models.SmallIntegerField(null=True, default=None, validators=[MinValueValidator(-100), MaxValueValidator(100)])
+    stealth_val = models.SmallIntegerField(null=True, default=None, validators=[MinValueValidator(-100), MaxValueValidator(100)])
+    survival_val = models.SmallIntegerField(null=True, default=None, validators=[MinValueValidator(-100), MaxValueValidator(100)])
 
 class CharacterClass(models.Model):
     """Model for representing the different class options available for player characters."""
     name = models.CharField(unique=True)
     # TODO: the rest of the class lmao
 
-class RawStats(models.Model):
-    """Model for holding the raw statistical modifiers for a character's abilities."""
-    strength     = models.PositiveSmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(20)])
-    constitution = models.PositiveSmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(20)])
-    dexterity    = models.PositiveSmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(20)])
-    wisdom       = models.PositiveSmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(20)])
-    intelligence = models.PositiveSmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(20)])
-    charisma     = models.PositiveSmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(20)])
+class CharacterSubclass(models.Model):
+    """Model for representing subclass options."""
+    name = models.CharField(unique=True)
+    superclass = models.ForeignKey(CharacterClass)
+    # TODO: the rest of the subclass teehee
 
-class Race(models.Model):
-    """Model that describes how races are stored in the database."""
-    name = models.CharField(unique=true)
-    size = models.CharField(max_length=1, choices=sizes.get_size_choices())
-    speed_walking = models.PositiveSmallIntegerField(validators=[validate_mod_five])
-    speed_flying = models.PositiveSmallIntegerField(validators=[validate_mod_five])
-    speed_burrowing = models.PositiveSmallIntegerField(validators=[validate_mod_five])
-    speed_swimming = models.PositiveSmallIntegerField(validators=[validate_mod_five])
-    speed_climbing = models.PositiveSmallIntegerField(validators=[validate_mod_five])
-    desc_short = models.CharField(max_length=500) # Arbitrary limit, may need revising later
-    desc_long = models.CharField()
-    age_desc = models.CharField()
+class ClassInstance(models.Model):
+    """Model for representing a particular instance of a class. For instance, this model may be used to store
+    information about a level 14 barbarian with a subclass in path of the bezerker."""
+    character = models.ForeignKey(Character, on_delete=models.CASCADE, null=False, blank=False)
+    class_type = models.ForeignKey(CharacterClass, on_delete=models.CASCADE, null=False, blank=False)
+    subclass_type = models.ForeignKey(CharacterSubclass, on_delete=models.CASCADE)
+    class_level = models.PositiveSmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(20)])
+
+
+
 
 class Choice(models.Model):
     """Model that descibes a choice which a player may have to make. For example, players choosing to make a Dwarf char-
@@ -154,8 +180,10 @@ class Option(models.Model):
     iencies, languages, ability score increases, and more."""
     items = models.ManyToManyField(models.Model, help_text="description of the contents of this option")
 
-class ItemProficiency(models.Model):
+class Proficiency(models.Model):
     proficiency_name = models.CharField()
+
+class ItemProficiency(Proficiency):
     item = models.ForeignKey(Item, on_delete=CASCADE)
 
 class Item(models.Model):
@@ -267,5 +295,22 @@ class MagicWeapon(MagicItem, Weapon):
     pass
 class WondrousItem(MagicItem):
     pass
+
+class Wand(MagicItem):
+    pass
+
 class MagicArmour(MagicItem, Armour):
     pass
+
+class Language(model.Models):
+    STANDARD = "S"
+    EXOTIC = "E"
+    type_choices = {
+        STANDARD : "Standard",
+        EXOTIC : "Exotic"
+    }
+
+    name = models.CharField()
+    desc = models.TextField()
+    type = models.CharField(choices=type_choices)
+    typical_speakers = models.CharField()
---
+++ srd_api_handler.py
@@ -0,0 +1,49 @@
+import requests
+
+def extract_indexes(response):
+    response = response.json()
+    indexes = []
+    for r in response["results"]:
+        indexes.append(r["index"])
+    return indexes
+
+def get_unique_instances_of_field(base_url, indexes, field):
+    payload = {}
+    headers = {
+        'Accept': 'application/json'
+    }
+
+    field_instances = []
+    count = 0
+    num_indexes = len(indexes)
+
+    for index in indexes:
+        url = base_url + "/" + index
+        response = requests.request("GET", url, headers=headers, data=payload).json()
+
+        count += 1
+
+        print(f"{count} / {num_indexes} retrieved")
+
+        if response[field] not in field_instances:
+            field_instances.append(response[field])
+
+    return field_instances
+
+url = "https://www.dnd5eapi.co/api/languages"
+
+payload = {}
+headers = {
+  'Accept': 'application/json'
+}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+# print(response.text)
+
+indexes = extract_indexes(response)
+
+# print(indexes)
+types = get_unique_instances_of_field(url, indexes, "type")
+
+print(types)
